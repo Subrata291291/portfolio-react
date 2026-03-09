073a085d79f80861f71fb08bd8a91e261fdff19b
Subrata Halder <subrata.haldar@codeclouds.com>
2026-03-10 00:09:33 +0530

    Added photoshop img

--- src/data/certificationsData.js
+++ src/data/certificationsData.js
@@ -1,73 +1,70 @@
 import wordpress from "../assets/images/wordpress.jpg";
 import wordpressLg from "../assets/images/wordpress.jpg";
+
 import figma from "../assets/images/figma.jpg";
 import figmaLg from "../assets/images/figma.jpg";
+
 import bootstrap from "../assets/images/bootstrap.jpg";
 import bootstrapLg from "../assets/images/bootstrap.jpg";
+
 import javascript from "../assets/images/javascript.jpg";
 import javascriptLg from "../assets/images/javascript.jpg";
+
 import jquery from "../assets/images/jquery.jpg";
 import jqueryLg from "../assets/images/jquery.jpg";
+
 import materialize from "../assets/images/materialize.jpg";
 import materializeLg from "../assets/images/materialize.jpg";
+
 import photoshop from "../assets/images/photoshop.jpg";
 import photoshopLg from "../assets/images/photoshop.jpg";
-
-       {/* https://www.udemy.com/certificate/UC-0a60e664-7f8e-4d99-b83e-c8fceb3cf1ff/ -wordpress
-       https://www.udemy.com/certificate/UC-27a6fd4a-b351-47bc-9128-436ae64b65c2/ -figma
-       https://www.udemy.com/certificate/UC-1b97965a-0548-4338-adc9-4fd81520f587/ -bootstrap
-       https://www.udemy.com/certificate/UC-944c48c4-29b9-41dc-8395-00d4d44d130a/ - javascript
-       https://www.udemy.com/certificate/UC-30f1a382-10df-472e-84a9-5e06e18aab2b/ -jquery
-       https://www.udemy.com/certificate/UC-52aafd5c-f099-43db-a926-0c3b4c4d732e/ - Materialize 
-       https://www.udemy.com/certificate/UC-fc30d719-4cfe-453a-8f55-80c7cc165a73/ -Photoshop  */}
-
 
 const certificationsData = [
   {
     id: 1,
-    title: "wordpress",
+    title: "WordPress",
     image: wordpress,
     preview: wordpressLg,
     live: "https://www.udemy.com/certificate/UC-0a60e664-7f8e-4d99-b83e-c8fceb3cf1ff/",
   },
   {
     id: 2,
-    title: "figma",
+    title: "Figma",
     image: figma,
     preview: figmaLg,
     live: "https://www.udemy.com/certificate/UC-27a6fd4a-b351-47bc-9128-436ae64b65c2/",
   },
   {
     id: 3,
-    title: "bootstrap",
+    title: "Bootstrap",
     image: bootstrap,
     preview: bootstrapLg,
     live: "https://www.udemy.com/certificate/UC-1b97965a-0548-4338-adc9-4fd81520f587/",
   },
   {
     id: 4,
-    title: "javascript",
+    title: "JavaScript",
     image: javascript,
     preview: javascriptLg,
     live: "https://www.udemy.com/certificate/UC-944c48c4-29b9-41dc-8395-00d4d44d130a/",
   },
   {
     id: 5,
-    title: "jquery",
+    title: "jQuery",
     image: jquery,
     preview: jqueryLg,
     live: "https://www.udemy.com/certificate/UC-30f1a382-10df-472e-84a9-5e06e18aab2b/",
   },
   {
     id: 6,
-    title: "materialize",
+    title: "Materialize",
     image: materialize,
     preview: materializeLg,
     live: "https://www.udemy.com/certificate/UC-52aafd5c-f099-43db-a926-0c3b4c4d732e/",
   },
   {
     id: 7,
-    title: "photoshop",
+    title: "Photoshop",
     image: photoshop,
     preview: photoshopLg,
     live: "https://www.udemy.com/certificate/UC-fc30d719-4cfe-453a-8f55-80c7cc165a73/",
