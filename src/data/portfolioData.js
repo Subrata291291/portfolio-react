5c0ebcbb60a34ba998d58458763000cd53fd2f60
Subrata Halder <subrata.haldar@codeclouds.com>
2026-03-12 13:06:58 +0530

    added makeup site link

--- src/data/portfolioData.js
+++ src/data/portfolioData.js
@@ -235,7 +235,7 @@ const portfolioData = [
   {
     id: 25,
     category: "blog",
-    title: "Music",
+    title: "audio",
     image: music,
     preview: musicLg,
     liveLink: "https://musicsbox.netlify.app/",
